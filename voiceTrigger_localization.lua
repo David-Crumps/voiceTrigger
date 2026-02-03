a8d178dfa8d70408063bdf849160f9b3be4e3dfe
David-Crumps <david.crumplin@gmail.com>
2026-02-03 14:00:07 +0200

    added localization

--- voiceTrigger_localization.lua
+++ voiceTrigger_localization.lua
@@ -1,5 +1,35 @@
+local mod = get_mod("voiceTrigger")
+
 return {
-	mod_description = {
-		en = "voiceTrigger description",
+	mod_name = {
+		en = "Voice Trigger"
 	},
+	mod_description = {
+		en = "Set keybinds for a number of voice lines"
+	},
+	vacuum_capsule = {
+		en = "Vacuum Capsule"
+	},
+	dog_hater = {
+		en = "Dog Hating (Hive Scum Only)"
+	},
+	im_falling = {
+		en = "Scream"
+	},
+	cryonic_rod = {
+		en = "Cryonic Rod"
+	},
+	no = {
+		en = "No!"
+	},
+	thank_you = {
+		en = "Thanks"
+	},
+	battery = {
+		en = "Battery"
+	},
+	psyker_scream = {
+		en = "Psychic Scream (Psyker Only)"
+	}
 }
+ 
